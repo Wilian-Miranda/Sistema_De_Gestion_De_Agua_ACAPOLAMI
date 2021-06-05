5249b8fc2e05129aa7c0c07bafd09aaf0d11571a
CarlosBenitez <78523221+0CarlosBenitez0@users.noreply.github.com>
2021-06-05 10:21:51 -0600

    Formulario de Actividad

---
+++ ACAPOLAMI/ACAPOLAMI/VISTA/frmActividad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACAPOLAMI.VISTA
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmActividad_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'cddActividad.Sucesos' Puede moverla o quitarla según sea necesario.
+            this.sucesosTableAdapter.Fill(this.cddActividad.Sucesos);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
