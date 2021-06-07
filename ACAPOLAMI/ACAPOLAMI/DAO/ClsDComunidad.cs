6ce2333b3ccddaf8ae29ce7ffd3809f9e170eb75
Keyri2020 <70116371+Keyri2020@users.noreply.github.com>
2021-06-07 11:43:46 -0600

    Agregando metodos

--- ACAPOLAMI/ACAPOLAMI/DAO/ClsDComunidad.cs
+++ ACAPOLAMI/ACAPOLAMI/DAO/ClsDComunidad.cs
@@ -1,10 +1,10 @@
-﻿
-using ACAPOLAMI.MODELO;
+﻿using ACAPOLAMI.MODELO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ACAPOLAMI.DAO
 {
@@ -20,5 +20,44 @@ namespace ACAPOLAMI.DAO
             }
             return lista;
         }
+
+        public void GuardarComunidades(string nombreComunidad)
+        {
+            try
+            {
+                using (ACAPOLAMIEntities db = new ACAPOLAMIEntities())
+                {
+                    db.sp_InsertarComunidad(nombreComunidad);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Se guardó existosamente");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        //public void ElimiarComunidades(int idComunidad)
+        //{
+        //    try
+        //    {
+        //        using (ACAPOLAMIEntities db = new ACAPOLAMIEntities())
+        //        {
+        //            db.sp_EliminarComunidad(idComunidad);
+        //            db.SaveChanges();
+
+        //            MessageBox.Show("La comunidad se eliminó exitosamente");
+        //        }
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
     }
 }
