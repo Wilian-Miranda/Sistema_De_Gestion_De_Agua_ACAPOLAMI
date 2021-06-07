1abfb24b4d43215ccd11db351870274cfaa2cac3
Keyri2020 <70116371+Keyri2020@users.noreply.github.com>
2021-06-07 11:33:16 -0600

    crud tb_estados

---
+++ ACAPOLAMI/ACAPOLAMI/DAO/ClsDEstados.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACAPOLAMI.MODELO;
+using System.Windows.Forms;
+
+namespace ACAPOLAMI.DAO
+{
+    class ClsDEstados
+    {
+        public List<Estados> MostrarEstados()
+        {
+            List<Estados> estados;
+
+            using (ACAPOLAMIEntities db = new ACAPOLAMIEntities())
+            {
+                estados = db.Estados.ToList();
+            }
+
+            return estados;
+        }
+
+        public void GuardarEstado(Estados nombre)
+        {
+            try
+            {
+                using (ACAPOLAMIEntities db = new ACAPOLAMIEntities())
+                {
+                    Estados tb_estados = new Estados();
+
+                    tb_estados.nombreEstado = nombre.nombreEstado;
+
+                    db.Estados.Add(tb_estados);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Se guardó exitosamente");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        public void EliminarEstado(int id)
+        {
+            try
+            {
+                using (ACAPOLAMIEntities db = new ACAPOLAMIEntities())
+                {
+                    int eliminar = Convert.ToInt32(id);
+                    Estados estado = db.Estados.Where(x => x.idEstado == eliminar).Select(x => x).FirstOrDefault();
+
+                    db.Estados.Remove(estado);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Se eliminó exitosamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
